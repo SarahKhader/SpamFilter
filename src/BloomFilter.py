bdc94838e4a1c6c55a481c2b4758250965d354da
sarahk <sarah_khader2008@yahoo.com>
2017-09-13 21:14:29 +0200

    initial git

---
+++ src/BloomFilter.py
@@ -0,0 +1,39 @@
+'''
+Created on Sep 13, 2017
+
+@author: sarah
+'''
+
+from bitarray import bitarray
+import mmh3
+
+class BloomFilter(set):
+
+    def __init__(self, size, hash_count):
+        super(BloomFilter, self).__init__()
+        self.bit_array = bitarray(size)
+        self.bit_array.setall(0)
+        self.size = size
+        self.hash_count = hash_count
+
+    def __len__(self):
+        return self.size
+
+    def __iter__(self):
+        return iter(self.bit_array)
+    
+    def BitArray(self):
+        return self.bit_array
+
+    def add(self, item):
+        for seed in range(self.hash_count):
+            index = mmh3.hash(item, seed) % self.size
+            self.bit_array[index] = 1
+        return self
+
+    def lookup(self, string):
+        for seed in range(self.hash_count):
+            result = mmh3.hash(string, seed) % self.size
+            if self.bit_array[result] == 0:
+                return False
+        return True
